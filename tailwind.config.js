1d7c966d4b3ee83ad897eb70c72a7b77dc7ee501
mhmdrzk <mhmdrzk2708@gmail.com>
2025-07-20 21:56:07 +0700

    menambahkan file tailwind.config.js  di project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: "#181a1c",
+      },
+    },
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"), // opsional
+  ],
+};
